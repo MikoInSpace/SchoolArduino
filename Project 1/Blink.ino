ac451a826c099faa981cfd595b16f23d12ae0c2b
Miko <mikoinspace@gmail.com>
2023-09-30 13:24:33 +0200

    Minor improvements

--- Project 1/Blink.ino
+++ Project 1/Blink.ino
@@ -4,46 +4,46 @@
 #define GREEN 2
 int AMOUNT = 5000;
 
-
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(4, OUTPUT);  // turn pin 4 to output
-  pinMode(3, OUTPUT);  // turn pin 3 to output
-  pinMode(2, OUTPUT);  // turn pin 2 to output
-  pinMode(A4, INPUT_PULLUP);
-  digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
-  digitalWrite(3, LOW);  // tu/home/mko/Downloads/Tetris - Tetris Main Theme.inorn the LED off by making the voltage LOW
-  digitalWrite(4, LOW);  // turn the LED off by making the voltage LOW
-  Serial.begin(9600);
+  pinMode(RED, OUTPUT);     // Set the LED pins as OUTPUT
+  pinMode(YELLOW, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(A4, INPUT_PULLUP); // Set A4 as an INPUT with pull-up resistor
+  digitalWrite(RED, LOW);   // Initialize LEDs to be off
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+  Serial.begin(9600);       // Initialize serial communication
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  if(!digitalRead(A4)); {
-   digitalWrite(YELLOW, LOW);
-   digitalWrite(GREEN, LOW);   // turn the LED off by making the voltage LOW
-   digitalWrite(RED, HIGH);    // turn the LED on (HIGH is the voltage level)
-   tone(BUZZERPIN, 700, 100);  // make tone on buzzer
-   delay(AMOUNT);              // wait for five seconds
-   digitalWrite(3, HIGH);      // turn the LED on (HIGH is the voltage level)
-   tone(BUZZERPIN, 700, 100);
-   delay(1000);                // wait for half a second
-   digitalWrite(YELLOW, LOW);  // turn the LED off by making the voltage LOW
-   digitalWrite(RED, LOW);     // turn the LED off by making the voltage LOW
-   digitalWrite(GREEN, HIGH);  // turn the LED on (HIGH is the voltage level)
-   tone(BUZZERPIN, 800, 500);  // play indicator tone
-   beep();                     // play beep function
-   delay(50);                  // wait for five seconds
-   digitalWrite(GREEN, LOW);
-   digitalWrite(YELLOW, HIGH);
-   delay(1000);
+  if (!digitalRead(A4)) {    // Check if the button on A4 is pressed
+    digitalWrite(YELLOW, LOW);  // Turn off yellow LED
+    digitalWrite(GREEN, LOW);   // Turn off green LED
+    digitalWrite(RED, HIGH);    // Turn on red LED
+    tone(BUZZERPIN, 700, 100);  // Make a tone on the buzzer
+    delay(AMOUNT);              // Wait for five seconds
+    digitalWrite(RED, LOW);     // Turn off red LED
+    delay(1000);                // Wait for one second
+    digitalWrite(YELLOW, HIGH); // Turn on yellow LED
+    tone(BUZZERPIN, 700, 100);  // Make a tone on the buzzer
+    delay(1000);                // Wait for one second
+    digitalWrite(YELLOW, LOW);  // Turn off yellow LED
+    digitalWrite(RED, LOW);     // Turn off red LED
+    digitalWrite(GREEN, HIGH);  // Turn on green LED
+    tone(BUZZERPIN, 800, 500);  // Play an indicator tone
+    beep();                     // Call the beep function
+    delay(50);                  // Wait for 50 milliseconds
+    digitalWrite(GREEN, LOW);   // Turn off green LED
+    digitalWrite(YELLOW, HIGH); // Turn on yellow LED
+    delay(1000);                // Wait for one second
   }
 }
 
-void beep() {                    //defined a variable for beeping after light turned green
-  for (int i = 0; i < 6; ++i) {  // loop
-    tone(BUZZERPIN, 200, 100);
-    delay(1000);
+void beep() {
+  for (int i = 0; i < 6; ++i) {  // Loop to make beeping sound
+    tone(BUZZERPIN, 200, 100);  // Make a beep on the buzzer
+    delay(200);                 // Wait for 200 milliseconds between beeps
   }
 }
